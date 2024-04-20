224b1af480e65f4b0ae547ff391c2deb5cf1b130
anpadoma <anpadoma@gmail.com>
2024-04-20 15:46:03 +0200

    20042024_1

---
+++ anpadoma/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var helloList = []string{
+	"Hello, world",
+	"Καλημέρα κόσμε",
+	"こんにちは世界",
+	"Привет, мир",
+}
+
+func hello(index int) (string, error) {
+	if index < 0 || index > len(helloList)-1 {
+		// Create an error, convert the int type to a string
+		return "", errors.New("out of range: " + strconv.Itoa(index))
+	}
+	return helloList[index], nil
+}
+
+func main() {
+	rand.NewSource(time.Now().UnixNano())
+	index := rand.Intn(len(helloList))
+	msg, err := hello(index)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(msg)
+}
